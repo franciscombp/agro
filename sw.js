84fbd734f037b6ee482b80bca35e6d4bbcc50af1
franciscombp <franciscombp@users.noreply.github.com>
2026-08-10 20:39:53 +0000

    deploy: 2b966044643bf5a2fb2b4121d661d1687cf329a2

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // APIs externas network-first con último dato guardado como respaldo.
 "use strict";
 
-const VERSION = "v5";
+const VERSION = "v6";
 const CACHE_SHELL = "mihuerto-shell-" + VERSION;
 const CACHE_RUNTIME = "mihuerto-runtime";
 
